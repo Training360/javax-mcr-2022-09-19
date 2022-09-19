f399267ad3497c4692cd3a4557016c795ae005f1
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-19 14:29:26 +0200

    Integrációs teszt

---
+++ src/test/java/training/employees/HelloControllerIT.java
@@ -0,0 +1,32 @@
+package training.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class HelloControllerIT {
+
+    @Autowired
+    HelloController helloController;
+
+    @MockBean
+    TimeMachine timeMachine;
+
+    @Test
+    void testSayHello() {
+        when(timeMachine.now()).thenReturn(LocalDateTime.parse("2022-01-01T10:00:00"));
+
+        var message = helloController.sayHello();
+//        assertTrue(message.startsWith("HELLO FROM SERVICE!"),
+//                "Doesn't start with the given message");
+        assertEquals("HELLO FROM SERVICE! 2022-01-01T10:00", message);
+    }
+}
