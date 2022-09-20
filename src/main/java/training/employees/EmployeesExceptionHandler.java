7c08a09282b6b2181bb5f632c8bfae4a6f2410bb
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-20 16:35:12 +0200

    Zalando Problem

---
+++ src/main/java/training/employees/EmployeesExceptionHandler.java
@@ -0,0 +1,29 @@
+package training.employees;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
+import org.zalando.problem.spring.web.advice.ProblemHandling;
+
+import java.net.URI;
+import java.util.UUID;
+
+@ControllerAdvice
+public class EmployeesExceptionHandler implements ProblemHandling {
+
+    @ExceptionHandler
+    ResponseEntity<Problem> handleException(EmployeeNotFoundException exception, NativeWebRequest request) {
+        Problem problem =
+                Problem.builder()
+                        .withType(URI.create("employees/employee-not-found"))
+                        .withTitle("Not found")
+                        .withStatus(Status.NOT_FOUND)
+                        .withDetail(exception.getMessage())
+                        .with("exceptionId", UUID.randomUUID().toString())
+                        .build();
+        return this.create(exception, problem, request);
+    }
+}
