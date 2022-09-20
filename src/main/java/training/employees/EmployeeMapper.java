59663b552760186300408e4637ad11cea17f257a
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-20 13:27:51 +0200

    MapStruct használata

---
+++ src/main/java/training/employees/EmployeeMapper.java
@@ -0,0 +1,14 @@
+package training.employees;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+
+import java.util.List;
+
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EmployeeMapper {
+
+    List<EmployeeDto> toDto(List<Employee> employees);
+
+    EmployeeDetailsDto toDto(Employee employee);
+}
--- src/main/java/training/employees/EmployeesService.java
+++ src/main/java/training/employees/EmployeesService.java
@@ -11,14 +11,19 @@ public class EmployeesService {
 
     private EmployeesRepository repository;
 
+    private EmployeeMapper employeeMapper;
+
     public List<EmployeeDto> listEmployees() {
-        return repository.findAll().stream()
-                .map(e -> new EmployeeDto(e.getId(), e.getName()))
-                .toList();
+//        return repository.findAll().stream()
+//                .map(e -> new EmployeeDto(e.getId(), e.getName()))
+//                .toList();
+        return employeeMapper.toDto(repository.findAll());
     }
 
     public EmployeeDetailsDto findEmployeeById(long id) {
-        var employee = repository.findById(id);
-        return new EmployeeDetailsDto(employee.getId(), employee.getName(), employee.getYearOfBirth());
+//        var employee = repository.findById(id);
+//        return new EmployeeDetailsDto(employee.getId(), employee.getName(), employee.getYearOfBirth());
+
+        return employeeMapper.toDto(repository.findById(id));
     }
 }
