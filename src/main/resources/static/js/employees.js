4bd40bb83e7d40443503e9c4a2977e09352272f7
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-21 13:32:04 +0200

    Táblázat rajzolás

--- src/main/resources/static/js/employees.js
+++ src/main/resources/static/js/employees.js
@@ -2,5 +2,13 @@ window.onload = function() {
     console.log("Starting employees frontend");
     fetch("http://localhost:8080/api/employees")
         .then(response => response.json())
-        .then(employees => console.log(employees));
+        .then(employees => printEmployees(employees));
+}
+
+function printEmployees(employees) {
+    tbody = document.querySelector("tbody");
+    for (employee of employees) {
+        row = `<tr><td>${employee.id}</td><td>${employee.name}</td></tr>`
+        tbody.innerHTML += row
+    }
 }
