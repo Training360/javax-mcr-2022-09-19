90a3379fc06f2b1dac18c3765d5f0f60787a60d7
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-21 13:47:47 +0200

    Frontend POST

--- src/main/resources/static/js/employees.js
+++ src/main/resources/static/js/employees.js
@@ -3,12 +3,28 @@ window.onload = function() {
     fetch("http://localhost:8080/api/employees")
         .then(response => response.json())
         .then(employees => printEmployees(employees));
+
+    const button = document.querySelector("button");
+    button.onclick =  function() {
+        console.log("Clicked")
+        const name = document.querySelector("#name").value;
+        const yearOfBirth = Number(document.querySelector("#yearOfBirth").value);
+        const data = {"name": name, "yearOfBirth": yearOfBirth}
+        console.log(data);
+
+        fetch("http://localhost:8080/api/employees", 
+                {method: 'POST', 
+                headers: {"Content-Type": "application/json"},  
+                body: JSON.stringify(data)})
+            .then(response => response.json())
+            .then(data => console.log(data));
+    }
 }
 
 function printEmployees(employees) {
     tbody = document.querySelector("tbody");
-    for (employee of employees) {
-        row = `<tr><td>${employee.id}</td><td>${employee.name}</td></tr>`
-        tbody.innerHTML += row
+    for (const employee of employees) {
+        row = `<tr><td>${employee.id}</td><td>${employee.name}</td></tr>`;
+        tbody.innerHTML += row;
     }
 }
