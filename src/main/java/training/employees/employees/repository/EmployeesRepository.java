8a9d13c062160834ade8de48304b7881542a2c47
Istvan Viczian <viczian.istvan@gmail.com>
2022-09-21 16:00:50 +0200

    Adatbáziskezelés

--- src/main/java/training/employees/employees/repository/EmployeesRepository.java
+++ src/main/java/training/employees/employees/repository/EmployeesRepository.java
@@ -2,11 +2,13 @@ package training.employees.employees.repository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.stereotype.Repository;
 import training.employees.employees.entity.Employee;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 import java.util.Optional;
 
@@ -18,14 +20,48 @@ public class EmployeesRepository {
 
     public List<Employee> findAll(Optional<String> prefix) {
         if (prefix.isEmpty()) {
-            return jdbcTemplate.query("select id, emp_name from employees",
+            return jdbcTemplate.query("select id, emp_name, year_of_birth from employees",
                     EmployeesRepository::mapEmployee);
         }
         else {
-            return jdbcTemplate.query("select id, emp_name from employees where emp_name like :prefix",
+            return jdbcTemplate.query("select id, emp_name, year_of_birth from employees where emp_name like ?",
                     EmployeesRepository::mapEmployee, prefix.get() + "%");
         }
     }
+
+    public Employee findById(long id) {
+        return jdbcTemplate.queryForObject("select id, emp_name, year_of_birth from employees where id = ?",
+                EmployeesRepository::mapEmployee, id);
+    }
+
+    public void save(Employee employee) {
+        var keyHolder = new GeneratedKeyHolder();
+
+        jdbcTemplate.update(connection -> {
+            var preparedStatement = connection.prepareStatement("insert into employees (emp_name, year_of_birth) values (?, ?)",
+                    Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, employee.getName());
+            preparedStatement.setInt(2, employee.getYearOfBirth());
+            return preparedStatement;
+        }, keyHolder);
+
+        employee.setId(keyHolder.getKey().longValue());
+    }
+
+    public Employee update(long id, int yearOfBirth) {
+        jdbcTemplate.update("update employees set year_of_birth = ? where id = ?",
+                yearOfBirth, id);
+        return findById(id);
+    }
+
+    public void delete(long id) {
+        jdbcTemplate.update("delete from employees where id = ?",
+                id);
+    }
+
+//    public void deleteAll() {
+//        employees.clear();
+//    }
 
     private static Employee mapEmployee(ResultSet rs, int i) throws SQLException {
         return new Employee(rs.getLong("id"),
@@ -33,29 +69,4 @@ public class EmployeesRepository {
                 rs.getInt("year_of_birth"));
     }
 
-    public Employee findById(long id) {
-        return employees.stream()
-                .filter(e -> e.getId() == id)
-                .findFirst()
-                .orElseThrow(() -> new EmployeeNotFoundException("Employee not found with id: " + id));
-    }
-
-    public void save(Employee employee) {
-        employee.setId(idGenerator.incrementAndGet());
-        employees.add(employee);
-    }
-
-    public Employee update(long id, int yearOfBirth) {
-        var employee = findById(id);
-        employee.setYearOfBirth(yearOfBirth);
-        return employee;
-    }
-
-    public void delete(long id) {
-        employees.removeIf(e -> e.getId() == id);
-    }
-
-//    public void deleteAll() {
-//        employees.clear();
-//    }
 }
