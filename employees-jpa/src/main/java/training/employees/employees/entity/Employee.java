447c7cdd62ff58bfffe4bf870330802b8d020cdd
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-03 13:37:16 +0200

    Tábla módosítások

--- employees-jpa/src/main/java/training/employees/employees/entity/Employee.java
+++ employees-jpa/src/main/java/training/employees/employees/entity/Employee.java
@@ -4,21 +4,20 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Table(name = "employees")
 public class Employee {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "emp_name")
     private String name;
 
     private int yearOfBirth;
