f61b4e61eb5ec82cbbb9ac89cc7d35400496ea53
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-03 09:58:44 +0200

    Aspektusok

--- src/main/java/training/employees/EmployeesApplication.java
+++ src/main/java/training/employees/EmployeesApplication.java
@@ -3,8 +3,10 @@ package training.employees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class EmployeesApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/training/employees/employees/service/LoggerAspect.java
@@ -0,0 +1,20 @@
+package training.employees.employees.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @Around("execution(* training.employees.employees.service.EmployeesService.*(..))")
+    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
+        log.info("Call method: {}", joinPoint.getSignature().getName());
+        return joinPoint.proceed();
+    }
+}
