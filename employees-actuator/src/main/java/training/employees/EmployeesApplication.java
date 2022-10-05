c9dd1b0c7ecc9d0372796546a8f027d42f3e1c52
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-05 14:55:04 +0200

    Actuatorok használata

--- employees-actuator/src/main/java/training/employees/EmployeesApplication.java
+++ employees-actuator/src/main/java/training/employees/EmployeesApplication.java
@@ -1,6 +1,8 @@
 package training.employees;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,6 +13,11 @@ public class EmployeesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeesApplication.class, args);
+	}
+
+	@Bean
+	public HttpTraceRepository httpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 
 }
