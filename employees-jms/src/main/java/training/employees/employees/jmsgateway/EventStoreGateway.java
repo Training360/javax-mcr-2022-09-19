9e5877a19e8233ca4f60de92e192b708e22fdd5a
Istvan Viczian <viczian.istvan@gmail.com>
2022-10-05 11:29:45 +0200

    JMS üzenet küldése

---
+++ employees-jms/src/main/java/training/employees/employees/jmsgateway/EventStoreGateway.java
@@ -0,0 +1,30 @@
+package training.employees.employees.jmsgateway;
+
+import lombok.AllArgsConstructor;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Component;
+
+import javax.jms.JMSException;
+import javax.jms.Message;
+
+@Component
+@AllArgsConstructor
+public class EventStoreGateway {
+
+    private JmsTemplate jmsTemplate;
+
+    public void sendEvent(String message) {
+        var json = String.format("""
+                {
+                    "message": %s
+                }
+                """, message);
+
+        jmsTemplate.convertAndSend(json, this::setHeader);
+    }
+
+    public Message setHeader(Message message) throws JMSException {
+        message.setStringProperty("_typeId", "CreateEventCommand");
+        return message;
+    }
+}
